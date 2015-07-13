cde224a28e1a0fc2e7426f47231f070e9c6f0e9c
hamedobaidy <hamed.obaidy@gmail.com>
2015-07-14 00:19:30 +0430

    new freesurface library project

---
+++ freesurface/include/libFreeSurface.h
@@ -0,0 +1,8 @@
+#ifndef libFreeSurface_H
+#define libFreeSurface_H
+
+#include "stdio.h"
+
+extern void print_hello ();
+
+#endif
---
+++ freesurface/libFreeSurface/libFreeSurface.c
@@ -0,0 +1,6 @@
+#include "libFreeSurface.h"
+
+void
+print_hello(){
+  printf("Hello World\n");
+}
